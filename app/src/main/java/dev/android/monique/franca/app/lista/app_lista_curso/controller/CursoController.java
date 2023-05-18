aa129217069c70a00d3ee048c75eb661b8aadd74
moniquefrancaTec <monique.franca.tec@gmail.com>
2023-05-18 20:54:23 -0300

    layout app

---
+++ app/src/main/java/dev/android/monique/franca/app/lista/app_lista_curso/controller/CursoController.java
@@ -0,0 +1,4 @@
+package dev.android.monique.franca.app.lista.app_lista_curso.controller;
+
+public class CursoController {
+}
---
+++ app/src/main/java/dev/android/monique/franca/app/lista/app_lista_curso/model/Curso.java
@@ -0,0 +1,4 @@
+package dev.android.monique.franca.app.lista.app_lista_curso.model;
+
+public class Curso {
+}
