dffed8840276cc220e3ab5b9d4c27660d7dddb5f
moniquefrancaTec <monique.franca.tec@gmail.com>
2023-05-19 20:13:43 -0300

    layout mais construtor-pessoas

--- app/src/main/java/dev/android/monique/franca/app/lista/app_lista_curso/MainActivity.java
+++ app/src/main/java/dev/android/monique/franca/app/lista/app_lista_curso/MainActivity.java
@@ -1,8 +1,10 @@
 package dev.android.monique.franca.app.lista.app_lista_curso;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static dev.android.monique.franca.app.lista.app_lista_curso.R.layout.activity_main;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import dev.android.monique.franca.app.lista.app_lista_curso.model.Pessoas_Curso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Pessoas_Curso pessoas = new Pessoas_Curso();
+        pessoas.setNomeAluno("Monique");
+        pessoas.setSobrenomeAluno("Franca");
+        pessoas.setNomeCursoDesejado("Fisica");
+        pessoas.setTelefoneContato("389999999");
+
+
+
     }
+
 }
