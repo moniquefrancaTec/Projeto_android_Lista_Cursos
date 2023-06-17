43468f1f93f219b84ccbc062f34736b96fdf519f
moniquefrancaTec <monique.franca.tec@gmail.com>
2023-06-16 21:27:24 -0300

    Tela splash-modelo1app

---
+++ app/src/main/java/dev/android/monique/franca/app/lista/app_lista_curso/view/splashActivity.java
@@ -0,0 +1,14 @@
+package dev.android.monique.franca.app.lista.app_lista_curso.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
